437703e26d6a3159623a2a2b487e3e7c39cb1925
freedom <freedom7341@hotmail.com>
2023-08-10 16:25:20 -0600

    delete old builds before zipping new ones just incase, rar still funny layout

--- batchcmp.bat
+++ batchcmp.bat
@@ -1,4 +1,6 @@
-7z a -r "%~dp0bin\bin.7z" "%~dp0bin\*.exe"
-WinRAR a -r "%~dp0bin\bin.rar" "%~dp0bin\*.exe"
+del /s "%~dp0\*.7z"
+del /s "%~dp0\*.rar"
+7z a -r "%~dp0\bin.7z" "%~dp0bin\*.exe"
+WinRAR a -r "%~dp0\bin.rar" "%~dp0bin\*.exe"
 del /s "%~dp0bin\*.exe"
 pause
