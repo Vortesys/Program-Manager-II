6302272d96abdd5a7ac93b46818cdf435e36be4c
freedom <freedom7341@hotmail.com>
2023-08-11 01:42:20 -0600

    and God said to the world
    "Let there be dialogs"
     - Gates 3:86

---
+++ progmgr/dialog.c
@@ -0,0 +1,16 @@
+/* * * * * * * *\
+	DIALOG.C -
+		Copyright (c) 2023 freedom7341, Vortesys
+	DESCRIPTION -
+		Program Manager's dialogs and related
+		common functions.
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+\* * * * * * * */
+
+/* Headers */
+#include "progmgr.h"
+#include "dialog.h"
+#include "resource.h"
+// #define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
---
+++ progmgr/dialog.h
@@ -0,0 +1,12 @@
+/* * * * * * * *\
+	DIALOG.H -
+		Copyright (c) 2023 freedom7341, Vortesys
+	DESCRIPTION -
+		Dialog function prototypes and related
+		common dialog functions.
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+\* * * * * * * */
+
+/* Pragmas */
+#pragma once
