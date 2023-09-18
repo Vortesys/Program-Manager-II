6943fc563b9dc40e13afd91e5171e3ad8f262ca7
freedom7341 <freedom7341@hotmail.com>
2023-09-18 11:13:16 -0600

    desktop rewrite coming soon

---
+++ progmgr/desktop2.c
@@ -0,0 +1,39 @@
+/* * * * * * * *\
+	DESKTOP2.C -
+		Copyright (c) 2023 Vortesys
+	DESCRIPTION -
+		Program Manager's desktop window.
+		Displays running programs.
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+\* * * * * * * */
+
+/* Headers */
+#include "progmgr.h"
+#include <Windows.h>
+
+/* Structures */
+typedef struct _DPI
+{
+	HWND hWnd;
+	HICON hIcon;
+	DWORD dwFlags;
+} DPI, * PDPI;
+
+/* * * *\
+	CreateDesktopWindow2 -
+		Creates the desktop window.
+	RETURNS -
+		TRUE if created successfully,
+		FALSE if otherwise.
+\* * * */
+BOOL CreateDesktopWindow2(VOID)
+{
+	// TODO:
+	// Create window, resize it to the size of the screen
+	// possibly create child windows for other monitors
+	// color the window invisible, get list of running
+	// programs and then draw them all and capture their
+	// system menus as context menus for each icon.
+	return TRUE;
+}
